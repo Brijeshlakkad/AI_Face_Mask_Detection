e872a752cd9126b2bf3c58968d285cfdfe9f5b64
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2021-12-04 01:13:34 -0500

    deleteNotRequiredImages for new_dataset

--- preprocess.py
+++ preprocess.py
@@ -15,13 +15,17 @@ columns = ["name", "x1", "x2", "y1", "y2", "classname"]
 if not os.path.isdir(root_dir):
     os.makedirs(root_dir)
 
+
 def get_df():
     input_file = "train.csv"
     return pd.read_csv(os.path.join(data_folder, input_file),
-                        skiprows=1, names=columns)
+                       skiprows=1, names=columns)
+
+
 def get_preprocessed_df():
     return pd.read_csv(os.path.join(root_dir, "preprocessed", "data.csv"),
-                        skiprows=1, names=["filename", "classname"])
+                       skiprows=1, names=["filename", "classname"])
+
 
 def make_dir(dir_path):
     if not os.path.isdir(dir_path):
@@ -32,10 +36,10 @@ def save_preselected_class_data(dir_path, file_name):
     make_dir(dir_path)
 
     preselected_classes = [
-    "face_with_mask",
-    "mask_colorful",
-    "face_no_mask",
-    "mask_surgical",
+        "face_with_mask",
+        "mask_colorful",
+        "face_no_mask",
+        "mask_surgical",
     ]
 
     selectedClassIndexes = []
@@ -54,19 +58,20 @@ def extract_same_class_files(src_dir, dir_path, p_class_name):
     from shutil import copyfile
 
     class_dir_path = os.path.join(dir_path, p_class_name)
-    
+
     make_dir(class_dir_path)
     copied = 0
     train_df = get_df()
     for row_index in range(len(train_df)):
         class_name = train_df[columns[5]][row_index]
         if class_name == p_class_name:
-            copyfile(os.path.join(src_dir, train_df[columns[0]][row_index]), os.path.join(class_dir_path, train_df[columns[0]][row_index]))
-            copied+=1
-    
+            copyfile(os.path.join(src_dir, train_df[columns[0]][row_index]), os.path.join(
+                class_dir_path, train_df[columns[0]][row_index]))
+            copied += 1
+
     print(f'{copied} files to {class_dir_path}')
 
-    
+
 def move_files_using_file(src_dir, class_dir_path, file_name):
     make_dir(class_dir_path)
     from shutil import copyfile
@@ -76,21 +81,25 @@ def move_files_using_file(src_dir, class_dir_path, file_name):
     # Using for loop
     for file in input_file:
         file = file.strip('\n')
-        copyfile(os.path.join(src_dir, file), os.path.join(class_dir_path, file))
-    
+        copyfile(os.path.join(src_dir, file),
+                 os.path.join(class_dir_path, file))
+
     # Closing files
     input_file.close()
+
 
 def move_files_using_list(src_dir, class_dir_path, l):
     make_dir(class_dir_path)
     from shutil import copyfile
 
     for file in l:
-        copyfile(os.path.join(src_dir, file), os.path.join(class_dir_path, file))
+        copyfile(os.path.join(src_dir, file),
+                 os.path.join(class_dir_path, file))
 
 
 def is_file(file_name):
     return file_name != '.DS_Store'
+
 
 def create_dataset(src_dir, dest_dir, external_data):
     import csv
@@ -100,17 +109,17 @@ def create_dataset(src_dir, dest_dir, external_data):
 
     train_df = get_df()
     preselected_classes = [
-    "mask_colorful",
-    "face_no_mask",
-    "mask_surgical",
+        "mask_colorful",
+        "face_no_mask",
+        "mask_surgical",
     ]
     extract_count = 400
     class_dict = []
     class_count_tracker = {
-        
+
     }
     processed_files = []
-    
+
     file_index = seed
     for row_index in range(len(train_df)):
         current_class_name = train_df[columns[5]][row_index]
@@ -123,11 +132,13 @@ def create_dataset(src_dir, dest_dir, external_data):
         file_name = train_df[columns[0]][row_index]
         if not is_file(file_name) or file_name in processed_files:
             continue
-        class_count_tracker[current_class_name]+=1
-        file_index+=1
+        class_count_tracker[current_class_name] += 1
+        file_index += 1
         new_file_name = str(file_index)+'.'+file_name.split(".")[-1]
-        copyfile(os.path.join(src_dir, file_name), os.path.join(dest_dir, new_file_name))
-        class_dict.append({"filename": new_file_name, "classname": current_class_name})
+        copyfile(os.path.join(src_dir, file_name),
+                 os.path.join(dest_dir, new_file_name))
+        class_dict.append({"filename": new_file_name,
+                          "classname": current_class_name})
         processed_files.append(file_name)
 
     if external_data is not None:
@@ -136,18 +147,21 @@ def create_dataset(src_dir, dest_dir, external_data):
         for file_name in os.listdir(data_path):
             if not is_file(file_name):
                 continue
-            file_index+=1
+            file_index += 1
             new_file_name = str(file_index)+'.'+file_name.split(".")[-1]
-            copyfile(os.path.join(data_path, file_name), os.path.join(dest_dir, new_file_name))
-            class_dict.append({"filename": new_file_name, "classname": classname})
+            copyfile(os.path.join(data_path, file_name),
+                     os.path.join(dest_dir, new_file_name))
+            class_dict.append(
+                {"filename": new_file_name, "classname": classname})
 
     # print(class_dict)
     with open(os.path.join(dest_dir, "..", "data.csv"), 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames = ['filename', 'classname'])
+        writer = csv.DictWriter(csvfile, fieldnames=['filename', 'classname'])
         writer.writeheader()
         writer.writerows(class_dict)
 
     print("File created: ", (file_index-seed))
+
 
 def preview_classes():
     classes = []
@@ -164,8 +178,25 @@ def preview_classes():
             classes.append(className)
         if className in preselected_class_count.keys():
             preselected_class_count[className] += 1
-    print(classes, preselected_class_count)           
+    print(classes, preselected_class_count)
 
+
+def deleteNotRequiredImages():
+    """Removes images that doesn't belong to new_dataset/train.csv"""
+    root_dir = os.path.join("data", "new_dataset")
+    dataframe = pd.read_csv(os.path.join(root_dir, "train.csv"), skiprows=1, names=[
+                            "name", "x1", "x2", "y1", "y2", "classname"])
+    deleted_files = 0
+    file_list = dataframe[columns[0]].to_list()
+    from shutil import copyfile
+    image_dir = os.path.join(root_dir, "images")
+    for file_name in os.listdir(image_dir):
+        if file_name not in file_list:
+            file_path = os.path.join(image_dir, file_name)
+            copyfile(file_path, os.path.join(root_dir, "not-required", file_name))
+            os.remove(file_path)
+            deleted_files += 1
+    print("Moved and deleted %s files" % deleted_files)
 
 
 # move_files_using_list(os.path.join(rootDir, 'preprocessed', 'face_with_mask'), os.path.join(rootDir, 'preprocessed', 'face_with_ff92_mask'), l)
@@ -175,6 +206,7 @@ def preview_classes():
 
 # preview_classes()
 
-create_dataset(os.path.join(data_folder, 'images'),
-    os.path.join(root_dir, 'preprocessed', 'images'),
-    {"path": os.path.join(root_dir, 'ffp2'), "classname": 'ffp2_mask'})
+# create_dataset(os.path.join(data_folder, 'images'),
+#     os.path.join(root_dir, 'preprocessed', 'images'),
+#     {"path": os.path.join(root_dir, 'ffp2'), "classname": 'ffp2_mask'})
+deleteNotRequiredImages()
